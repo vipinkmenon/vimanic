68689cea16b95a1470603e2cbc537f922e2adf1b
Vipin K <vipinkmenon@users.noreply.github.com>
2022-06-24 11:11:25 +0530

    Update adrv9361.h

--- adrv_lvds/sw/spiTest/src/adrv9361.h
+++ adrv_lvds/sw/spiTest/src/adrv9361.h
@@ -1,5 +1,16 @@
+#ifndef ADRV9361_H
+#define ADRV9361_H
+
+
 #include "xparameters.h"
+#include "xil_types.h"
+
 
 
 int initADRV();
 void adrvSoftReset();
+void dumpAllReg();
+void findRegChange();
+
+
+#endif
